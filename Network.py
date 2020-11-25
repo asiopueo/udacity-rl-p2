ce13ba916e039cc550bde88bdf0421dea0e6a692
Martin <martin.lippl@pueo.de>
2020-11-25 20:08:27 +0100

    Basic network implementations for actor and critic

--- Network.py
+++ Network.py
@@ -14,7 +14,7 @@ from keras.optimizers import Adam
 import tensorflow.keras.losses as losses
 
 
-def network_simple():
+def network_actor():
     model = Sequential()
     # The network is used to approximate a POLICY. As a result, we suggest the following structure:
     # 
@@ -40,3 +40,29 @@ def network_simple():
     model.compile(loss=losses.mean_squared_error, optimizer='sgd')
     return model
 
+
+def network_critic():
+    model = Sequential()
+    # The network is used to approximate a POLICY. As a result, we suggest the following structure:
+    # 
+    # Since the observation space consists of 33 variables corresponding to position, rotation, 
+    # velocity, and angular velocities of the arm, the input layer shall consist of 33 neurons
+    #
+    # The output layer consists of two neurons for four floating point numbers
+    # between -1 and 1 which represent the torque applied to the two joints of
+    # the robot arm
+
+    model.add( Dense(33, input_shape=(33,) ) )
+    model.add( Activation('relu') )
+    model.add( Dropout(0.2) )
+    model.add( Dense(64) )
+    model.add( Activation('relu') )
+    model.add( Dropout(0.2) )
+    model.add( Dense(64) )
+    model.add( Activation('relu') )
+    model.add( Dropout(0.2) )
+    model.add( Dense(1) )
+    model.add( Activation('tanh') )
+
+    model.compile(loss=losses.mean_squared_error, optimizer='sgd')
+    return model
