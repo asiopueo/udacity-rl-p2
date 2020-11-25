4795b6734db2d88d736444cd3038b22c47ff16fb
Martin <martin.lippl@pueo.de>
2020-11-25 10:52:54 +0100

    Some very basic adjustment to the network to adapt it to the problem at hand

--- Network.py
+++ Network.py
@@ -11,17 +11,21 @@ from keras.layers import MaxPooling2D, Flatten, BatchNormalization
 from keras.layers import Dense, Dropout, Activation
 from keras.layers.convolutional import Convolution2D
 from keras.optimizers import Adam
-#from tf.keras.utils import np_utils, to_categorical # ???
 import tensorflow.keras.losses as losses
 
 
 def network_simple():
     model = Sequential()
-    # The input layer consists of 37 neurons (35 rays + 2 velocity)
-    # The angles are emanated as follows:
-    # [,,,,,,]
+    # The network is used to approximate a POLICY. As a result, we suggest the following structure:
+    # 
+    # Since the observation space consists of 33 variables corresponding to position, rotation, 
+    # velocity, and angular velocities of the arm, the input layer shall consist of 33 neurons
+    #
+    # The output layer consists of two neurons for four floating point numbers
+    # between -1 and 1 which represent the torque applied to the two joints of
+    # the robot arm
 
-    model.add( Dense(37, input_shape=(37,) ) )
+    model.add( Dense(33, input_shape=(33,) ) )
     model.add( Activation('relu') )
     model.add( Dropout(0.2) )
     model.add( Dense(64) )
@@ -36,26 +40,3 @@ def network_simple():
     model.compile(loss=losses.mean_squared_error, optimizer='sgd')
     return model
 
-
-
-
-"""
-def network_pixel():
-    model = Sequential()
-
-    model.add( Convolution2D(32, (3,3), input_shape=(), padding='valid', strides=(1,1), activation='relu', data_format='channels_first' )
-    model.add( BatchNormalization() )
-    model.add( MaxPooling2D( pool_size=(2,2), data_format='channels_first' ) )
-    model.add( Convolution2D(64, (3,3), input_shape=(), padding='valid', strides=(1,1), activation='relu', data_format='channels_first' )
-    model.add( BatchNormalization() )
-    model.add( MaxPooling2D( pool_size=(2,2), data_format='channels_first') )
-
-    model.add( Flatten() )
-
-    model.add( Dense() )
-    model.add( Dense() )
-    model.add( Dense(4) )
-
-    model.compile(loss='categorical_crossentropy', optimizer='adam')
-    return model
-"""
