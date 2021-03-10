16df407e337d3cdc1f1a966a2133dc6730d966f7
Martin <martin.lippl@pueo.de>
2021-03-10 11:07:37 +0100

    Defined new training()-function which encapsulates the training loops

--- train.py
+++ train.py
@@ -36,81 +36,88 @@ agent = Agent(buffer_size=10000, batch_size=64, gamma=0.98, epsilon=0.1, action_
 #  Main learning loop:
 ####################################
 
-# Initial values:
 
-episode = 0
-score = 0           
-tick = 0
+def training(n_episodes=500):
+    score = 0           
+    tick = 0
 
-score_list = []
-score_trailing_list = deque(maxlen=10)
+    score_list = []
+    score_trailing_list = deque(maxlen=10)
 
-eps = 1.0
-eps_rate = 0.995
-eps_end = 0.02
-
-
-#agent.load_weights("./checkpoints")
-
-for episode in range(0, 300):
-    ticks = 0
-    score = 0
-
-    env_info = env.reset(train_mode=True)[brain_name]  # Reset the environment
-    state = env_info.vector_observations[0]             # Get the current state
-
-    start = time.time()
-    while True:
-        # Select action according to policy:
-        action = agent.action(state, eps, add_noise=True)
-
-        print('Action taken: ', action, 'Time: ', tick)
-
-        # Take action and record the reward and the successive state
-        env_info = env.step(action)[brain_name]
-        
-        reward = env_info.rewards[0]
-        next_state = env_info.vector_observations[0]
-        done = env_info.local_done[0]
-
-        # Add experience to the agent's replay buffer:
-        exp = Experience(state, action, reward, next_state, done)
-        agent.replay_buffer.insert_into_buffer( exp )
-        
-        agent.learn()
-
-        score += reward
-        state = next_state
-        
-        eps = max( eps_rate*eps, eps_end )
-
-        if done is True:
-            break
-
-        ticks += 1
+    eps = 1.0
+    eps_rate = 0.995
+    eps_end = 0.02
 
 
-    end = time.time()
+    #agent.load_weights("./checkpoints")
 
-    score_list.append(score)
-    score_trailing_list.append(score)
+    for episode in range(0, 300):
+        ticks = 0
+        score = 0
 
-    score_avg = np.mean(score_list)
-    score_trailing_avg = np.mean(score_trailing_list)
+        env_info = env.reset(train_mode=True)[brain_name]  # Reset the environment
+        state = env_info.vector_observations[0]             # Get the current state
 
-    print("***********************************************")
-    print("Score of episode {}: {}".format(episode, score))
-    print("Avg. score: {:.2f}".format(score_avg))
-    print("Trailing avg. score: {:.2f}".format(score_trailing_avg))
-    print("Greedy epsilon used: {}".format(eps))
-    print("Time consumed: {:.2f} s".format(end-start))
-    print("***********************************************")
+        start = time.time()
+        while True:
+            # Select action according to policy:
+            action = agent.action(state, eps, add_noise=True)
+
+            print('Action taken: ', action, 'Time: ', tick)
+
+            # Take action and record the reward and the successive state
+            env_info = env.step(action)[brain_name]
+            
+            reward = env_info.rewards[0]
+            next_state = env_info.vector_observations[0]
+            done = env_info.local_done[0]
+
+            # Add experience to the agent's replay buffer:
+            exp = Experience(state, action, reward, next_state, done)
+            agent.replay_buffer.insert_into_buffer( exp )
+            
+            agent.learn()
+
+            score += reward
+            state = next_state
+            
+            eps = max( eps_rate*eps, eps_end )
+
+            if done is True:
+                break
+
+            ticks += 1
 
 
-    print("Total score:", score)
-    agent.save_weights("./checkpoints")
+        end = time.time()
 
-    episode += 1
+        score_list.append(score)
+        score_trailing_list.append(score)
+
+        score_avg = np.mean(score_list)
+        score_trailing_avg = np.mean(score_trailing_list)
+
+        print("***********************************************")
+        print("Score of episode {}: {}".format(episode, score))
+        print("Avg. score: {:.2f}".format(score_avg))
+        print("Trailing avg. score: {:.2f}".format(score_trailing_avg))
+        print("Greedy epsilon used: {}".format(eps))
+        print("Time consumed: {:.2f} s".format(end-start))
+        print("***********************************************")
+
+
+        print("Total score:", score)
+        agent.save_weights("./checkpoints")
+
+        episode += 1
+
+    return score_list, score_trailing_list
+
+
+
+training()
+
+
 
 env.close()
 
